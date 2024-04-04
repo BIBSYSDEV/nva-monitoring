03a0c7fd44c44b36ce463785051a8599f1f09168
anetteOlli <anette.olli.siiri@sikt.no>
2024-04-04 18:06:42 +0200

    listen-to-stack-deployed, log4j problems

--- dashboard/src/main/java/no/sikt/nva/monitoring/UpdateDashboardHandler.java
+++ dashboard/src/main/java/no/sikt/nva/monitoring/UpdateDashboardHandler.java
@@ -4,17 +4,19 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.CloudFormationCustomResourceEvent;
 import com.amazonaws.services.lambda.runtime.logging.LogLevel;
-import java.lang.ProcessHandle.Info;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UpdateDashboardHandler implements RequestHandler<CloudFormationCustomResourceEvent, Void> {
+
     private static final Logger logger = LoggerFactory.getLogger(UpdateDashboardHandler.class);
 
+    public UpdateDashboardHandler() {
+
+    }
 
     @Override
     public Void handleRequest(CloudFormationCustomResourceEvent cloudFormationCustomResourceEvent, Context context) {
-        context.getLogger().log("Fudge", LogLevel.INFO);
         logger.info("Hello world");
         logger.warn("Try again");
         logger.error("ERRROR");

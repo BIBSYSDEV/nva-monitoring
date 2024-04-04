a4a536719121ecc4f15bd3b466fd2bbae3965153
anetteOlli <anette.olli.siiri@sikt.no>
2024-04-04 11:23:02 +0200

    listen-to-stack-deployed, empty lambda created

---
+++ dashboard/src/main/java/no/sikt/nva/monitoring/UpdateDashboardHandler.java
@@ -0,0 +1,25 @@
+package no.sikt.nva.monitoring;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.CloudFormationCustomResourceEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UpdateDashboardHandler implements RequestHandler<CloudFormationCustomResourceEvent, Void> {
+    private static final Logger logger = LoggerFactory.getLogger(UpdateDashboardHandler.class);
+
+
+    @Override
+    public Void handleRequest(CloudFormationCustomResourceEvent cloudFormationCustomResourceEvent, Context context) {
+        logger.info("Hello world");
+        //Future plans:
+        //List all lambdas in the account
+        //List all alarms in the account
+        //Create cloudwatch widgets for lambdas
+        //Create cloudwatch widgets for alarm
+        //Attach widgets to cloudwatch dashboard.
+        //Update sharing policy? and share dashboard
+        return null;
+    }
+}

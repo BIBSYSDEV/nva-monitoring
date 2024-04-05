ac6f8debf786ca03e914a94fe72f80c1b529a6f1
anetteOlli <44363838+anetteOlli@users.noreply.github.com>
2024-04-05 12:42:19 +0200

    Listen to stack deployed (#1)
    * listen-to-stack-deployed, empty lambda created
    
    * listen-to-stack-deployed, added buildspec.yaml
    
    * listen-to-stack-deployed, added checkstyle and pmd config
    
    * listen-to-stack-deployed, template.yml fix
    
    * listen-to-stack-deployed, template.yml fix
    
    * listen-to-stack-deployed, template.yml fix
    
    * listen-to-stack-deployed, template.yml fix
    
    * listen-to-stack-deployed, template.yml fix
    
    * listen-to-stack-deployed, template.yml fix 2
    
    * listen-to-stack-deployed, template.yml, trying to fix getting triggered by stack update
    
    * listen-to-stack-deployed, template.yml, trying to fix getting triggered by stack update
    
    * listen-to-stack-deployed, fixing troubles with logs
    
    * listen-to-stack-deployed, fixing troubles with logs
    
    * listen-to-stack-deployed, fixing troubles with logs
    
    * listen-to-stack-deployed, fixing troubles with logs
    
    * listen-to-stack-deployed, fixing troubles with logs
    
    * listen-to-stack-deployed, fixing troubles with logs
    
    * listen-to-stack-deployed, fixing troubles with logs
    
    * listen-to-stack-deployed, fixing troubles with logs
    
    * listen-to-stack-deployed, fixing troubles with logs
    
    * listen-to-stack-deployed, fixing troubles with logs
    
    * listen-to-stack-deployed, log4j problems
    
    * listen-to-stack-deployed, config from aws
    
    * listen-to-stack-deployed, config from aws
    
    * listen-to-stack-deployed, config from aws
    
    * listen-to-stack-deployed, cleanup dependencies
    
    * listen-to-stack-deployed, cleanup dependencies
    
    * listen-to-stack-deployed, log retention time
    
    * listen-to-stack-deployed, log retention time
    
    * listen-to-stack-deployed, code-review
    
    * listen-to-stack-deployed, reverting policy

---
+++ dashboard/src/main/java/no/sikt/nva/monitoring/UpdateDashboardHandler.java
@@ -0,0 +1,29 @@
+package no.sikt.nva.monitoring;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.CloudFormationCustomResourceEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UpdateDashboardHandler implements RequestHandler<CloudFormationCustomResourceEvent, Void> {
+
+    private static final Logger logger = LoggerFactory.getLogger(UpdateDashboardHandler.class);
+
+    public UpdateDashboardHandler() {
+
+    }
+
+    @Override
+    public Void handleRequest(CloudFormationCustomResourceEvent cloudFormationCustomResourceEvent, Context context) {
+        logger.info("Trigges ved stack updates");
+        //Future plans:
+        //List all lambdas in the account
+        //List all alarms in the account
+        //Create cloudwatch widgets for lambdas
+        //Create cloudwatch widgets for alarm
+        //Attach widgets to cloudwatch dashboard.
+        //Update sharing policy? and share dashboard
+        return null;
+    }
+}

68f65a5a79d11b861f8f9deee122e84aa21ea768
Kir Truhacev <kir.truhacev@sikt.no>
2024-08-02 13:03:33 +0200

    changing log widget size

--- dashboard/src/main/java/no/sikt/nva/monitoring/model/factory/LogWidgetFactory.java
+++ dashboard/src/main/java/no/sikt/nva/monitoring/model/factory/LogWidgetFactory.java
@@ -18,17 +18,13 @@ public class LogWidgetFactory {
 
     public static final String LOG = "log";
     public static final String AWS_LAMBDA_LOG_GROUP_PREFIX = "/aws/lambda/";
-    private final CloudWatchLogsClient cloudWatchLogsClient;
-    private final LambdaClient lambdaClient;
+    private final List<String> logGroups;
 
     public LogWidgetFactory(CloudWatchLogsClient cloudWatchLogsClient, LambdaClient lambdaClient) {
-        this.cloudWatchLogsClient = cloudWatchLogsClient;
-        this.lambdaClient = lambdaClient;
-
+        this.logGroups = fetchNewestLambdaLogGroups(lambdaClient, cloudWatchLogsClient);
     }
 
     public CloudWatchWidget<LogProperties> createLogWidget(String title, String filter) {
-        var logGroups = fetchNewestLambdaLogGroups(this.lambdaClient, cloudWatchLogsClient);
         var query = constructQueryForLogGroups(logGroups, filter);
         var logProperties = LogProperties.builder()
                                 .withRegion(Region.EU_WEST_1.toString())
@@ -36,7 +32,7 @@ public class LogWidgetFactory {
                                 .withView("table")
                                 .withQuery(query)
                                 .build();
-        return new CloudWatchWidget<>(LOG, logProperties, 6, 12, 12, 24);
+        return new CloudWatchWidget<>(LOG, logProperties, 6, 24, 12, 24);
     }
 
     private static String constructQueryForLogGroups(List<String> logGroups, String filter) {
@@ -51,11 +47,11 @@ public class LogWidgetFactory {
 
     private static ArrayList<String> fetchNewestLambdaLogGroups(LambdaClient lambdaClient,
                                                                 CloudWatchLogsClient cloudWatchLogsClient) {
-        var functions = fetchLamdaFunctions(lambdaClient);
+        var lambdaFunctionNames = fetchLamdaFunctions(lambdaClient);
         var logGroups = new ArrayList<String>();
-        for (FunctionConfiguration function : functions) {
+        for (String functionName : lambdaFunctionNames) {
 
-            var logGroupsRequest = createDescribeLambdaLogGroupsRequest(function);
+            var logGroupsRequest = createDescribeLambdaLogGroupsRequest(functionName);
 
             cloudWatchLogsClient.describeLogGroups(logGroupsRequest).logGroups().stream()
                 .max(Comparator.comparing(LogGroup::creationTime))
@@ -65,13 +61,17 @@ public class LogWidgetFactory {
         return logGroups;
     }
 
-    private static DescribeLogGroupsRequest createDescribeLambdaLogGroupsRequest(FunctionConfiguration function) {
+    private static DescribeLogGroupsRequest createDescribeLambdaLogGroupsRequest(String functionName) {
         return DescribeLogGroupsRequest.builder()
-                   .logGroupNamePrefix(AWS_LAMBDA_LOG_GROUP_PREFIX + function.functionName())
+                   .logGroupNamePrefix(AWS_LAMBDA_LOG_GROUP_PREFIX + functionName)
                    .build();
     }
 
-    private static List<FunctionConfiguration> fetchLamdaFunctions(LambdaClient lambdaClient) {
-        return lambdaClient.listFunctions(ListFunctionsRequest.builder().build()).functions();
+    private static List<String> fetchLamdaFunctions(LambdaClient lambdaClient) {
+        return lambdaClient.listFunctions(ListFunctionsRequest.builder().build())
+                   .functions()
+                   .stream()
+                   .map(FunctionConfiguration::functionName)
+                   .toList();
     }
 }

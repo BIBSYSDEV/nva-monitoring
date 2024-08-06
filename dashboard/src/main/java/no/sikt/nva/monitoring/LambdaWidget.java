2432f906fbe90b7f98652ba871e5f874a06ce717
Kir Truhacev <kir.truhacev@sikt.no>
2024-08-06 08:55:13 +0200

    aligning lambda widget on the top

--- dashboard/src/main/java/no/sikt/nva/monitoring/LambdaWidget.java
+++ dashboard/src/main/java/no/sikt/nva/monitoring/LambdaWidget.java
@@ -7,33 +7,52 @@ import software.amazon.awssdk.regions.Region;
 public final class LambdaWidget {
 
     public static final String AWS_LAMBDA = "AWS/Lambda";
+    public static final String ERRORS = "Errors";
+    public static final String THROTTLES = "Throttles";
+    public static final String INVOCATIONS = "Invocations";
+    public static final String CONCURRENT_EXECUTIONS = "ConcurrentExecutions";
+    public static final String CLAIMED_ACCOUNT_CONCURRENCY = "ClaimedAccountConcurrency";
+    public static final String ASYNC_EVENT_AGE = "AsyncEventAge";
+    public static final String LAMBDA_METRICS_TITLE = "Lambda metrics - last 5 minutes";
+    public static final String SINGLE_VALUE = "singleValue";
+    public static final String SUM_STAT = "Sum";
+    public static final int PERIOD_5_MINUTES = 300;
+    public static final String METRIC = "metric";
+    public static final String MAXIMUM_STAT = "Maximum";
 
     private LambdaWidget() {
     }
 
     public static CloudWatchWidget<LambdaProperties> create() {
         var lambdaProperties = LambdaProperties.builder()
-                                   .withMetrics(List.of(List.of(AWS_LAMBDA, "Errors", lambdaMetricWithRegion()),
-                                                        List.of(AWS_LAMBDA, "Throttles", lambdaMetricWithRegion()),
-                                                        List.of(AWS_LAMBDA, "Invocations", lambdaMetricWithRegion()),
-                                                        List.of(AWS_LAMBDA, "ConcurrentExecutions",
-                                                                lambdaMetricWithRegionAndMaxStat()),
-                                                        List.of(AWS_LAMBDA, "ClaimedAccountConcurrency",
-                                                                lambdaMetricWithRegionAndMaxStat()),
-                                                        List.of(AWS_LAMBDA, "AsyncEventAge",
-                                                                LambdaMetric.builder().withStat("Maximum").build())))
-                                   .withTitle("Lambda metrics - last 5 minutes")
-                                   .withView("singleValue")
+                                   .withMetrics(getAwsLambdaMetricEntry())
+                                   .withTitle(LAMBDA_METRICS_TITLE)
+                                   .withView(SINGLE_VALUE)
                                    .withRegion(Region.EU_WEST_1.toString())
-                                   .withStat("Sum")
-                                   .withPeriod(300)
+                                   .withStat(SUM_STAT)
+                                   .withPeriod(PERIOD_5_MINUTES)
                                    .withStacked(true)
                                    .withSparkline(true)
                                    .withTrend(true)
                                    .withLiveData(true)
                                    .withSingleValueFullPrecision(true)
                                    .build();
-        return new CloudWatchWidget<LambdaProperties>("metric", lambdaProperties, 4, 24, 0, 29);
+        return new CloudWatchWidget<LambdaProperties>(METRIC, lambdaProperties, 4, 24, 5, 0);
+    }
+
+    private static List<List<Object>> getAwsLambdaMetricEntry() {
+        return List.of(
+            createAwsLambdaMetricEntry(ERRORS, lambdaMetricWithRegion()),
+            createAwsLambdaMetricEntry(THROTTLES, lambdaMetricWithRegion()),
+            createAwsLambdaMetricEntry(INVOCATIONS, lambdaMetricWithRegion()),
+            createAwsLambdaMetricEntry(CONCURRENT_EXECUTIONS, lambdaMetricWithRegionAndMaxStat()),
+            createAwsLambdaMetricEntry(CLAIMED_ACCOUNT_CONCURRENCY, lambdaMetricWithRegionAndMaxStat()),
+            createAwsLambdaMetricEntry(ASYNC_EVENT_AGE, lambdaMetricWithRegionAndMaxStat())
+        );
+    }
+
+    private static List<Object> createAwsLambdaMetricEntry(String type, LambdaMetric lambdaMetric) {
+        return List.of(AWS_LAMBDA, type, lambdaMetric);
     }
 
     private static LambdaMetric lambdaMetricWithRegion() {
@@ -41,6 +60,6 @@ public final class LambdaWidget {
     }
 
     private static LambdaMetric lambdaMetricWithRegionAndMaxStat() {
-        return LambdaMetric.builder().withRegion(Region.EU_WEST_1.toString()).withStat("Maximum").build();
+        return LambdaMetric.builder().withRegion(Region.EU_WEST_1.toString()).withStat(MAXIMUM_STAT).build();
     }
 }
